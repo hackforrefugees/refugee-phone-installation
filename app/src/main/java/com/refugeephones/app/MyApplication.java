b7913afb4cc7f9e4503dc205d72d89c3b93b826e
Waqas Aslam <waqas@interaktionsbyran.se>
2015-12-05 13:28:41 +0100

    *wrapper classes

---
+++ app/src/main/java/com/refugeephones/app/MyApplication.java
@@ -0,0 +1,18 @@
+package com.refugeephones.app;
+
+import android.app.Application;
+
+/**
+ * Mother of all... It's the base application
+ * Created by Waqas on 005 05-Dec-15.
+ */
+public class MyApplication extends Application {
+    private final String TAG = "MyApplication";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+
+}
